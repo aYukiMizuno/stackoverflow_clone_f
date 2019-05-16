346275caf4e6692cbdb76c7f9f516befc7c18298
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-16 17:59:55 +0900

    Implement showQuestion

--- web/controller/question/show.ex
+++ web/controller/question/show.ex
@@ -1,7 +1,28 @@
 defmodule StackoverflowCloneF.Controller.Question.Show do
   use StackoverflowCloneF.Controller.Application
 
-  def show(_conn) do
-    # Implement me
+  alias StackoverflowCloneF.Helper.ErrorJson
+  alias StackoverflowCloneF.Error
+  alias StackoverflowCloneF.Controller.Question
+
+  def show(%Antikythera.Conn{request: %Antikythera.Request{path_matches: %{id: id}}} = conn) do
+
+    req = Dodai.RetrieveDedicatedDataEntityRequest.new(
+      StackoverflowCloneF.Dodai.default_group_id(),
+      "Question",
+      id,
+      StackoverflowCloneF.Dodai.root_key()
+      )
+
+    res = Sazabi.G2gClient.send(
+      conn.context,
+      StackoverflowCloneF.Dodai.app_id(),
+      req)
+
+    case res do
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: data} -> Conn.json(conn, 200, Question.Helper.to_response_body(data))
+      %Dodai.ResourceNotFound{} -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
+    end
+
   end
 end
