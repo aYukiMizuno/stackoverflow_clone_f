f182f090e5de82dc987cfc407cc67998aa523f0b
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-16 17:55:20 +0900

    Implement createQuestion

--- web/controller/question/create.ex
+++ web/controller/question/create.ex
@@ -1,7 +1,35 @@
 defmodule StackoverflowCloneF.Controller.Question.Create do
   use StackoverflowCloneF.Controller.Application
 
-  def create(_conn) do
-    # Implement me
+  alias StackoverflowCloneF.Error
+  alias StackoverflowCloneF.Controller.Question
+
+  def create(conn) do
+
+    credential = conn.request.headers["authorization"]
+
+    req = Dodai.CreateDedicatedDataEntityRequest.new(
+      StackoverflowCloneF.Dodai.default_group_id(),
+      "Question",
+      credential,
+      %Dodai.CreateDedicatedDataEntityRequestBody{data: %{
+        "title" => conn.request.body["title"],
+        "body" => conn.request.body["body"]
+        }}
+      )
+
+    res = Sazabi.G2gClient.send(
+      conn.context,
+      StackoverflowCloneF.Dodai.app_id(),
+      req)
+
+    case res do
+      %Dodai.CreateDedicatedDataEntitySuccess{body: data} -> Conn.json(conn, 200, Question.Helper.to_response_body(data))
+      %Dodai.BadRequest{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+      %Dodai.AuthenticationError{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+    end
+
   end
 end
+
+
