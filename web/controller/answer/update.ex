68981f6b000c7d01e0bb40caeb539ca263fe3f88
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-17 18:09:13 +0900

    update update.ex （nocheck）

--- web/controller/answer/update.ex
+++ web/controller/answer/update.ex
@@ -1,7 +1,82 @@
 defmodule StackoverflowCloneF.Controller.Answer.Update do
   use StackoverflowCloneF.Controller.Application
+  alias StackoverflowCloneF.Dodai, as: SD
+  alias StackoverflowCloneF.Error
+  alias StackoverflowCloneF.Controller.Question
+  # alias StackoverflowCloneF.Controller.Book.Helper
 
-  def update(_conn) do
-    # Implement me
+  plug StackoverflowCloneF.Plug.FetchMe, :fetch, []
+
+  defmodule CommentCroma do
+    use Croma.Struct, fields: [
+      body: Croma.String
+    ]
   end
+
+
+  def update(conn) do
+    # Implement me
+
+    question_id = "TODO: question-id"
+
+    user_id = conn.assigns.me["_id"]
+
+
+    validate(conn, fn (body) ->
+      check_question(conn, question_id, user_id, fn () ->
+
+        data = %{
+          # "user_id"     => user_id,     # login userの`_id`
+          # "question_id" => question_id, # userが指定した値
+          "body"        => %{"$set" => body},        # userが指定した値
+          # "comments"    => [],          # 最初は必ず空配列
+        }
+
+        req = Dodai.UpdateDedicatedDataEntityRequest.new(
+          SD.default_group_id(),
+          "Answer",
+          question_id,
+          SD.root_key(),
+          %Dodai.UpdateDedicatedDataEntityRequestBody{data: %{"$set" => data}} # , owner: user_id
+          )
+
+        res = Sazabi.G2gClient.send(
+          conn.context,
+          SD.app_id(),
+          req)
+
+        case res do
+          %Dodai.UpdateDedicatedDataEntitySuccess{body: data} -> Conn.json(conn, 200, Question.Helper.to_response_body(data))
+          %Dodai.BadRequest{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+          %Dodai.AuthenticationError{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+          _ -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+        end
+
+      end)
+    end)
+
+  end
+
+  def validate(conn, f) do
+    case CommentCroma.new(conn.request.body) do
+      {:ok, %CommentCroma{body: body}} -> f.(body)
+      _ -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+    end
+  end
+
+  def check_question(conn, question_id, user_id, f) do
+    req = Dodai.RetrieveDedicatedDataEntityRequest.new(SD.default_group_id(), "Question", question_id, SD.root_key())
+    res = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
+
+    case res do
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: %{"data" => %{"user_id" => given_user_id}}} ->
+        case given_user_id do
+          ^user_id -> f.()
+          _ -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+        end
+      %Dodai.ResourceNotFound{}                   -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
+    end
+
+  end
+
 end
