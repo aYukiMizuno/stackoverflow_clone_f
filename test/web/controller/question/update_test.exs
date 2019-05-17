c21bf0d3c7b6e8561eb0b91eede4abff675660f6
longniu <niu.long@access-company.com>
2019-05-17 16:29:17 +0900

    編集中、テストまたエラーがある

--- test/web/controller/question/update_test.exs
+++ test/web/controller/question/update_test.exs
@@ -1,7 +1,38 @@
 defmodule StackoverflowCloneF.Controller.Question.UpdateTest do
   use StackoverflowCloneF.CommonCase
+  
+  @api_prefix "/v1/question/question_id"
+  @header     %{}
+  @body       %{"title" => "new title", "body" => "new body",}
 
   test "update/1 " do
-    # Implement me
+
+    mock_fetch_me_plug(%{"_id" => "user_id"})
+
+    :meck.expect(StackoverflowCloneF.Plug.FetchMe, :fetch, fn(conn, _) ->
+      SolomonLib.Conn.assign(conn, :me, StackoverflowCloneF.TestData.UserData.dodai())
+    end)
+
+    :meck.expect(Sazabi.G2gClient, :send, fn(_, _, req) ->
+      case req do
+        %Dodai.RetrieveDedicatedDataEntityRequest{} = retrieve_req ->
+          # retrieve_reqについて必要に応じてassertする
+          assert retrieve_req.id == "question_id"
+          %Dodai.RetrieveDedicatedDataEntitySuccess{body: StackoverflowCloneF.TestData.QuestionData.dodai()}
+        %Dodai.UpdateDedicatedDataEntityRequest{}   = update_req   ->
+          # update_reqについての必要に応じてassertする
+          assert update_req.body == %Dodai.UpdateDedicatedDataEntityRequestBody{
+            data: %{"$set" => %{"title" => "new title", "body" => "new body",}}
+          }
+          
+          
+          %Dodai.UpdateDedicatedDataEntitySuccess{body: StackoverflowCloneF.TestData.QuestionData.dodai()}
+      end
+    end)
+
+    res = Req.put_json(@api_prefix, @body, @header)
+    assert res.status               == 200
+    assert Poison.decode!(res.body) == StackoverflowCloneF.TestData.QuestionData.gear()
+
   end
 end
