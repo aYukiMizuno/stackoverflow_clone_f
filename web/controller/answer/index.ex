8b694ad47c50c41e862d1e6a716d9fd96cae0849
longniu <niu.long@access-company.com>
2019-05-20 15:15:37 +0900

    Implemented Answer Index API

--- web/controller/answer/index.ex
+++ web/controller/answer/index.ex
@@ -1,7 +1,45 @@
+use Croma
+
+defmodule StackoverflowCloneF.Controller.Answer.IndexRequestParams do
+  use Croma.Struct, fields: [
+    user_id:  Croma.TypeGen.nilable(Croma.String),
+    question_id: Croma.TypeGen.nilable(Croma.String),
+  ]
+end
+
+
 defmodule StackoverflowCloneF.Controller.Answer.Index do
   use StackoverflowCloneF.Controller.Application
+  alias Sazabi.G2gClient
+  alias StackoverflowCloneF.Dodai, as: SD
+  alias StackoverflowCloneF.Error.BadRequestError
+  alias StackoverflowCloneF.Controller.Answer.{Helper, IndexRequestParams}
 
-  def index(_conn) do
-    # Implement me
+  # def index(conn)do
+  #   IO.inspect conn
+  # end
+  defun index(%Conn{request: %Request{query_params: query_params}, context: context} = conn :: v[Conn.t]) :: Conn.t do
+    IO.inspect conn
+    case IndexRequestParams.new(query_params) do
+      {:error, _}      ->
+        ErrorJson.json_by_error(conn, BadRequestError.new())
+      {:ok, validated} ->
+        query = convert_to_dodai_req_query(validated)
+        req = Dodai.RetrieveDedicatedDataEntityListRequest.new(SD.default_group_id(), Helper.collection_name(), SD.root_key(), query)
+        %Dodai.RetrieveDedicatedDataEntityListSuccess{body: docs} = G2gClient.send(context, SD.app_id(), req)
+        Conn.json(conn, 200, Enum.map(docs, &Helper.to_response_body(&1)))
+    end
+  end
+
+  defunpt convert_to_dodai_req_query(params :: v[IndexRequestParams.t]) :: Dodai.RetrieveDedicatedDataEntityListRequestQuery.t do
+    query = Map.from_struct(params)
+    |> Enum.reject(fn {_, value} -> is_nil(value) end)
+    |> Enum.map(fn {k, v} -> {"data.#{k}", v} end)
+    |> Map.new()
+
+    %Dodai.RetrieveDedicatedDataEntityListRequestQuery{
+      query: query,
+      sort:  %{"_id" => 1}
+    }
   end
 end
