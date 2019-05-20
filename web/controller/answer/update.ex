e85914e2f9c871ade9fc4b0afe925bd9255acc47
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-20 10:58:12 +0900

    add updateAnswer

--- web/controller/answer/update.ex
+++ web/controller/answer/update.ex
@@ -14,28 +14,24 @@ defmodule StackoverflowCloneF.Controller.Answer.Update do
   end
 
 
-  def update(conn) do
-    # Implement me
-
-    question_id = "TODO: question-id"
+  def update(%Antikythera.Conn{request: %Antikythera.Request{path_matches: %{id: answer_id}}} = conn) do
 
     user_id = conn.assigns.me["_id"]
 
-
     validate(conn, fn (body) ->
-      check_question(conn, question_id, user_id, fn () ->
+      check_answer(conn, answer_id, user_id, fn () ->
 
         data = %{
           # "user_id"     => user_id,     # login userの`_id`
           # "question_id" => question_id, # userが指定した値
-          "body"        => %{"$set" => body},        # userが指定した値
+          "body"        => body,          # userが指定した値
           # "comments"    => [],          # 最初は必ず空配列
         }
 
         req = Dodai.UpdateDedicatedDataEntityRequest.new(
           SD.default_group_id(),
           "Answer",
-          question_id,
+          answer_id,
           SD.root_key(),
           %Dodai.UpdateDedicatedDataEntityRequestBody{data: %{"$set" => data}} # , owner: user_id
           )
@@ -47,15 +43,16 @@ defmodule StackoverflowCloneF.Controller.Answer.Update do
 
         case res do
           %Dodai.UpdateDedicatedDataEntitySuccess{body: data} -> Conn.json(conn, 200, Question.Helper.to_response_body(data))
-          %Dodai.BadRequest{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
-          %Dodai.AuthenticationError{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
-          _ -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+          # %Dodai.BadRequest{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+          # %Dodai.AuthenticationError{} -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+          _ -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
         end
 
       end)
     end)
 
   end
+
 
   def validate(conn, f) do
     case CommentCroma.new(conn.request.body) do
@@ -64,17 +61,19 @@ defmodule StackoverflowCloneF.Controller.Answer.Update do
     end
   end
 
-  def check_question(conn, question_id, user_id, f) do
-    req = Dodai.RetrieveDedicatedDataEntityRequest.new(SD.default_group_id(), "Question", question_id, SD.root_key())
+
+  def check_answer(conn, answer_id, user_id, f) do
+    req = Dodai.RetrieveDedicatedDataEntityRequest.new(SD.default_group_id(), "Answer", answer_id, SD.root_key())
     res = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
 
     case res do
       %Dodai.RetrieveDedicatedDataEntitySuccess{body: %{"data" => %{"user_id" => given_user_id}}} ->
         case given_user_id do
           ^user_id -> f.()
-          _ -> ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+          # %Dodai.ResourceNotFound{} -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
+          _ -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
         end
-      %Dodai.ResourceNotFound{}                   -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
+      _ -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
     end
 
   end
