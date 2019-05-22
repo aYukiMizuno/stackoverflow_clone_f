0f1249396cff87e7e3646b35fe538ac55eda15bb
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-22 11:31:48 +0900

    implement createVote

--- web/controller/vote/create.ex
+++ web/controller/vote/create.ex
@@ -10,7 +10,6 @@ defmodule StackoverflowCloneF.Controller.Vote.Create do
 
   def create(%Antikythera.Conn{request: %Antikythera.Request{path_matches: %{id: question_id}}} = conn) do
 
-    IO.inspect conn.request.path_info
     collection_name = case conn.request.path_info do
       [_, "answer" | _] -> "Answer"
       [_, "question" | _] -> "Question"
@@ -20,60 +19,55 @@ defmodule StackoverflowCloneF.Controller.Vote.Create do
       [_, _, _, "vote", "dislike_vote"] -> ["dislike_voter_ids", "like_voter_ids"]
     end
 
-    check_id(conn, question_id, collection_name, fn () ->
+    check_id(conn, question_id, collection_name, fn (%{"data" => data} = _body) ->
 
       user_id = conn.assigns.me["_id"]
 
-      req1 = Dodai.UpdateDedicatedDataEntityRequest.new(
-        SD.default_group_id(),
-        collection_name,
-        question_id,
-        SD.root_key(),
-        %Dodai.UpdateDedicatedDataEntityRequestBody{data: %{"$addToSet" => %{positive_list_name => user_id}}})
+      if (data[positive_list_name] |> Enum.any?(&(&1 == user_id))) do
+        ErrorJson.json_by_error(conn, Error.BadRequestError.new())
 
-      res1 = Sazabi.G2gClient.send(
-        conn.context,
-        SD.app_id(),
-        req1)
+      else
 
+        req1 = Dodai.UpdateDedicatedDataEntityRequest.new( # TODO: 重複してもエラーにならない
+          SD.default_group_id(),
+          collection_name,
+          question_id,
+          SD.root_key(),
+          %Dodai.UpdateDedicatedDataEntityRequestBody{data: %{
+            "$addToSet" => %{positive_list_name => user_id},
+            "$pull" => %{negative_list_name => user_id},
+          }}
+        )
 
-      req2 = Dodai.UpdateDedicatedDataEntityRequest.new(
-        SD.default_group_id(),
-        collection_name,
-        question_id,
-        SD.root_key(),
-        %Dodai.UpdateDedicatedDataEntityRequestBody{data: %{"$pull" => %{negative_list_name => user_id}}})
+        res1 = Sazabi.G2gClient.send(
+          conn.context,
+          SD.app_id(),
+          req1
+        )
 
-      res2 = Sazabi.G2gClient.send(
-        conn.context,
-        SD.app_id(),
-        req2)
+        case res1 do
+          %Dodai.UpdateDedicatedDataEntitySuccess{body: data1}  ->
+            Conn.json(conn, 200, to_response_body(data1))
+          _ ->
+            ErrorJson.json_by_error(conn, Error.BadRequestError.new())
+        end
 
-      case res1 do
-        %Dodai.UpdateDedicatedDataEntitySuccess{body: data1}  ->
-          case res2 do
-            %Dodai.UpdateDedicatedDataEntitySuccess{body: data2} ->
-              Conn.json(conn, 200, to_response_body(data2))
-            _ ->
-              Conn.json(conn, 200, to_response_body(data1))
-          end
-        _ ->
-          ErrorJson.json_by_error(conn, Error.BadRequestError.new())
       end
 
     end)
 
   end
 
-  def check_id(conn, id, collection_name, f) do
+
+  defunp check_id(conn :: v[Conn.t], id :: String, collection_name :: String, f) :: Conn.t do
     req = Dodai.RetrieveDedicatedDataEntityRequest.new(SD.default_group_id(), collection_name, id, SD.root_key())
     res = Sazabi.G2gClient.send(conn.context, SD.app_id(), req)
-
     case res do
-      %Dodai.RetrieveDedicatedDataEntitySuccess{} -> f.()
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: body} -> f.(body)
       %Dodai.ResourceNotFound{} -> ErrorJson.json_by_error(conn, Error.ResourceNotFoundError.new())
     end
   end
+
 
   defunp to_response_body(doc :: map) :: map do
     %{
