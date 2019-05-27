b9acf9bd51c8d52a3ac837b75cc861c420a1df11
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-27 10:16:59 +0900

    Implement postCommentOfQuestion

--- web/static/components/Question.vue
+++ web/static/components/Question.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="page-title">
+    <h2 class="page-title"> <!-- TODO: デザインをなんとかする -->
       {{ question.title }}
     </h2>
     <p class="body">
@@ -13,6 +13,29 @@
     >
       <comment :comment="comment" />
     </div>
+    <div>
+      <form
+        class="form-group"
+        @submit.prevent="submit_postComment"
+      >
+
+        <div class="form-group">
+          <label for="exampleInputEmail1">コメント</label>
+
+          <input
+            class="form-control"
+            v-model="bodyOfComment"
+            placeholder="コメント欄"
+            type="text"
+            minlength="1"
+            required
+          />
+        </div>
+        <button class="btn btn-primary" type="submit">
+          投稿
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -21,8 +44,24 @@ import Comment from '@/components/Comment';
 
 export default {
   name: 'Question',
+  data(){
+    return {
+      bodyOfComment: ''
+    }
+  },
   components: {
     Comment,
+  },
+  methods:{
+    submit_postComment(){
+      this.$store.dispatch('createQuestionComment',{
+        questionId: this.question.id,
+        body: this.bodyOfComment
+      })
+      .then( ()=> {
+        // TODO: 
+      });
+    }
   },
   props: {
     question: {
