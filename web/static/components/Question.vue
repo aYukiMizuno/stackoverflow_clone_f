910f767161cc8e9ae1d98415743080cc096381d2
longniu <niu.long@access-company.com>
2019-05-27 13:18:22 +0900

    質問のvueを修正中、とりあえず更新の見る形をできした

--- web/static/components/Question.vue
+++ web/static/components/Question.vue
@@ -1,11 +1,77 @@
 <template>
   <div>
-    <h2 class="page-title"> <!-- TODO: デザインをなんとかする -->
-      {{ question.title }}
-    </h2>
-    <p class="body">
-      {{ question.body }}
-    </p>
+    <div v-if="editing">
+      <div class="form-group">
+        <label for="form-title">タイトル</label>
+        <input
+          id="form-title"
+          v-model="editingTitle"
+          maxlength="100"
+          minlength="1"
+          class="title-edit form-control"
+          type="text"
+          required
+        >
+      </div>
+    </div>
+    <div v-else>
+      <h2 class="page-title"> <!-- TODO: デザインをなんとかする -->
+        {{ question.title }}
+      </h2>
+    </div>
+    <div class="content-area">
+      <div v-if="editing">
+        <form
+          class="question-form"
+          @submit.prevent="update"
+        >
+          <div class = "form-group">
+            <label for="form-body">内容</label>
+            <input
+              id="form-body"
+              v-model="editingBody"
+              class="body-edit form-control"
+              maxlength="3000"
+              minlength="1"
+              type="text"
+              required
+            >
+            </div>
+              <div class="form-group">
+                <button
+                  class="btn btn-primary mb-2"
+                  type="submit"
+                >
+                  保存
+                </button>
+                <button
+                  class="cancel-edit-button btn btn-outline-primary mb-2"
+                  type="submit"
+                  @click.prevent="cancelEdit"
+                >
+                  キャンセル
+                </button>
+              </div>
+        </form>
+      </div>
+      <div v-else>
+        <div class="body">
+          {{ question.body }}
+        </div>
+        <div class="additional">
+          <span v-if="!editing">
+            <button
+              type="button"
+              class="edit-button btn btn-link"
+              @click="startEdit"
+            >
+              更新
+            </button>
+          </span>
+        </div>
+      </div>
+    </div>
+    
     <div
       v-for="comment in question.comments"
       :key="comment.id"
@@ -46,13 +112,28 @@ export default {
   name: 'Question',
   data(){
     return {
-      bodyOfComment: ''
+      editing: false,
+      editingTitle: '',
+      editingBody: '',
+      bodyOfComment: '',
     }
   },
   components: {
     Comment,
   },
   methods:{
+    startEdit(){
+      this.editing = true;
+      this.editingTitle = this.question.title;
+      this.editingBody = this.question.body;
+    },
+    cancelEdit(){
+      this.editing = false;
+    },
+    update(){
+      this.$emit('update',{title:editingTitle, body: this.editingBody});
+      this.editing = false;
+    },
     submit_postComment(){
       this.$store.dispatch('createQuestionComment',{
         questionId: this.question.id,
