05c64a5a67dd847bc199b12fcfe8d29529d6f5eb
longniu <niu.long@access-company.com>
2019-05-27 17:26:28 +0900

    更新機能を実装中

--- web/static/components/Comment.vue
+++ web/static/components/Comment.vue
@@ -1,23 +1,97 @@
 <template>
   <div class="comment">
-    <div class="comment-user">
-      {{ comment.userId }} さん
+    <div v-if="editingQC">
+      <div class = "form-comment-group">
+        <form
+          class="question-comment-form"
+        >
+          <label for="form-comment-group">コメント</label>
+          <input
+            id="form-comment-body"
+            class="comment-body-edit form-control"
+            v-model="editingQCBody"
+            maxlength="1000"
+            minlength="1"
+            type="text"
+            required
+          >
+          <div class="form-comment-group">
+            <button
+              class="btn btn-primary mb-2"
+              type="submit"
+              @click.prevent="submit_upqueComment"
+            >
+              保存
+            </button>
+            <button
+              class="cancel-edit-button btn btn-outline-primary mb-2"
+              type="submit"
+              @click.prevent="cancelQCEdit"
+            >
+              キャンセル
+            </button>
+          </div>
+        </form>
+        
+      </div>
     </div>
-    <p class="comment-body">
-      {{ comment.body }}
-    </p>
+    <div v-else>
+      <div class="comment-user">
+      {{ comment.userId }} さん
+      </div>
+      <p class="comment-body">
+        {{ comment.body }}
+      </p>  
+      <span v-if="!editingQC">
+        <button
+          type="button"
+          class="qc-edit-button btn btn-link"
+          @click.prevent="startQCEdit"
+        >
+          コメント更新
+        </button>
+      </span>
+        
   </div>
+    </div>
+    
 </template>
 
 <script>
 export default {
   name: 'Comment',
+  data(){
+    return {
+      editingQC: false,
+      editingQCBody:'', 
+    }
+  },
   props: {
     comment: {
       type: Object,
       required: true,
     },
   },
+  methods:{
+    startQCEdit(){
+      this.editingQC = true;
+      this.editingQCBody = this.comment.body;
+    },
+    cancelQCEdit(){
+      this.editingQC = false;
+    },
+    submit_upqueComment(){
+      this.$store.dispatch('updateQuestionComment',{
+        questionId: this.$route.params.document_id, 
+        id: this.$route.params.comment_id,
+        body: this.editingQCBody});
+
+        // console.log(this.$route)
+        this.editingQC = false;
+    },
+
+  },
+
 };
 </script>
 
