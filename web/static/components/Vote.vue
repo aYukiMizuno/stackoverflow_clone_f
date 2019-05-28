341302738e00b40d84690cf57bc3989edbc3a602
longniu <niu.long@access-company.com>
2019-05-28 17:57:20 +0900

    voteのUIをデザインした,またv-ifでアイコン変更の課題が残る

--- web/static/components/Vote.vue
+++ web/static/components/Vote.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="vote">
-    <button @click="likeVote()">
-      すき
+  <div class="footer">
+    <button type="button" class="btn btn-outline-light text-dark" data-toggle="tooltip" data-placement="bottom" title="いいね" @click="likeVote()">
+      <i v-if="isLikeVoted" class="fas fa-thumbs-up" style="font-size:20px"></i>
+      <i v-else class="far fa-thumbs-up" style="font-size:20px"></i>  {{ countLikeVote }}
+    </button >
+    <button type="button" class="btn btn-outline-light text-dark" data-toggle="tooltip" data-placement="bottom" title="よくない" @click="dislikeVote()">
+      <i v-if="isDislikeVoted" class="fas fa-thumbs-down" style="font-size:20px"></i>
+      <i v-else class="far fa-thumbs-down" style="font-size:20px"></i>  {{ countDislikeVote }}
     </button>
-    <span>{{ countVote }}</span>
-    <button @click="dislikeVote()">
-      きらい
-    </button>
+    
+    <!-- <span>{{ countVote }}</span> -->
   </div>
 </template>
 
@@ -20,19 +23,37 @@ export default {
     },
   },
   computed: {
+    isLikeVoted(){
+      return this.document.likeVoterIds.indexOf(this.$store.state.id)>=0;
+    },
+    isDislikeVoted(){
+      return this.document.dislikeVoterIds.indexOf(this.$store.state.id)>=0;
+    },
     countVote() {
       return this.document.likeVoterIds.length - this.document.dislikeVoterIds.length;
     },
+    countLikeVote(){
+      //console.log(this.document.likeVoterIds.indexOf(this.$store.state.id));
+      return this.document.likeVoterIds.length;
+    },
+    countDislikeVote(){
+      //console.log(this.document.dislikeVoterIds.indexOf(this.$store.state.id));
+      return this.document.dislikeVoterIds.length;
+    }
   },
   methods: {
     likeVote() {
-      this.$store.dispatch('createQuestionVote', { questionId: this.document.id, voteType: 'like_vote' });
+      this.$store.dispatch('createQuestionVote', { questionId: this.document.id, voteType: 'like_vote' })
+      .then(()=>{
+        console.log(this.isLikeVoted);
+      });
     },
     dislikeVote() {
       this.$store.dispatch('createQuestionVote', { questionId: this.document.id, voteType: 'dislike_vote' });
     },
   },
 };
+
 </script>
 
 <style scoped>
